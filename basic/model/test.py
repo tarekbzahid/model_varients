b1c0b6fccbadb5a34ec4655d38df354f45f103ea
Tarek Z <bonie_tg@outlook.com>
2024-05-28 10:54:45 -0700

    commit

--- basic/model/test.py
+++ basic/model/test.py
@@ -5,15 +5,15 @@ import numpy as np
 from utils.utils_ import log_string, metric
 from utils.utils_ import load_data
 
-# Assuming device is your CUDA device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
 def test(args, log):
+    # Set the device
+    device = torch.device(torch.device('cuda', args.cuda_device) if torch.cuda.is_available() else "cpu")
+
+    # Load data
     (trainX, trainTE, trainY, valX, valTE, valY, testX, testTE,
      testY, SE, mean, std) = load_data(args)
-    
-    
+
+    # Move data to device
     if torch.cuda.is_available():
         trainX = trainX.to(device)
         trainTE = trainTE.to(device)
@@ -24,11 +24,11 @@ def test(args, log):
         testX = testX.to(device)
         testTE = testTE.to(device)
         testY = testY.to(device)
-        SE= SE.to(device)
+        SE = SE.to(device)
         mean = mean.to(device)
         std = std.to(device)
 
-
+    # Get data shapes
     num_train, _, num_vertex = trainX.shape
     num_val = valX.shape[0]
     num_test = testX.shape[0]
@@ -36,47 +36,45 @@ def test(args, log):
     val_num_batch = math.ceil(num_val / args.batch_size)
     test_num_batch = math.ceil(num_test / args.batch_size)
 
+    # Load model
     model = torch.load(args.model_file)
 
-    # test model
-    log_string(log, '**** testing model ****')
-    log_string(log, 'loading model from %s' % args.model_file)
+    # Test model
+    log_string(log, '**** Testing model ****')
+    log_string(log, 'Loading model from %s' % args.model_file)
     model = torch.load(args.model_file)
-    log_string(log, 'model restored!')
-    log_string(log, 'evaluating...')
+    log_string(log, 'Model restored!')
+    log_string(log, 'Evaluating...')
 
     with torch.no_grad():
+        # Initialize predictions
+        trainPred = torch.empty(0, device=device)
+        valPred = torch.empty(0, device=device)
+        testPred = torch.empty(0, device=device)
 
-        #trainPred = []
-        trainPred = torch.empty(0, device=device)  
+        # Evaluate on training data
         for batch_idx in range(train_num_batch):
             start_idx = batch_idx * args.batch_size
             end_idx = min(num_train, (batch_idx + 1) * args.batch_size)
             X = trainX[start_idx: end_idx]
             TE = trainTE[start_idx: end_idx]
             pred_batch = model(X, TE)
-            #trainPred.append(pred_batch.detach().clone())
             trainPred = torch.cat((trainPred, pred_batch.detach().clone()), dim=0)
             del X, TE, pred_batch
         trainPred = trainPred * std + mean
 
-
-        #valPred = []
-        valPred = torch.empty(0, device=device)
+        # Evaluate on validation data
         for batch_idx in range(val_num_batch):
             start_idx = batch_idx * args.batch_size
             end_idx = min(num_val, (batch_idx + 1) * args.batch_size)
             X = valX[start_idx: end_idx]
             TE = valTE[start_idx: end_idx]
             pred_batch = model(X, TE)
-            #valPred.append(pred_batch.detach().clone())
             valPred = torch.cat((valPred, pred_batch.detach().clone()), dim=0)
             del X, TE, pred_batch
-        #valPred = torch.from_numpy(np.concatenate(valPred, axis=0))
         valPred = valPred * std + mean
 
-        #testPred = []
-        testPred = torch.empty(0, device=device)
+        # Evaluate on test data
         start_test = time.time()
         for batch_idx in range(test_num_batch):
             start_idx = batch_idx * args.batch_size
@@ -84,35 +82,38 @@ def test(args, log):
             X = testX[start_idx: end_idx]
             TE = testTE[start_idx: end_idx]
             pred_batch = model(X, TE)
-            #testPred.append(pred_batch.detach().clone())
             testPred = torch.cat((testPred, pred_batch.detach().clone()), dim=0)
             del X, TE, pred_batch
-        #testPred = torch.from_numpy(np.concatenate(testPred, axis=0))
-        testPred = testPred* std + mean
+        testPred = testPred * std + mean
     end_test = time.time()
-    
+
+    # Calculate metrics
     train_mae, train_rmse, train_mape = metric(trainPred, trainY)
     val_mae, val_rmse, val_mape = metric(valPred, valY)
     test_mae, test_rmse, test_mape = metric(testPred, testY)
-    log_string(log, 'testing time: %.1fs' % (end_test - start_test))
+
+    # Log testing time
+    log_string(log, 'Testing time: %.1fs' % (end_test - start_test))
     log_string(log, '                MAE\t\tRMSE\t\tMAPE')
-    log_string(log, 'train            %.2f\t\t%.2f\t\t%.2f%%' %
+    log_string(log, 'Train            %.2f\t\t%.2f\t\t%.2f%%' %
                (train_mae, train_rmse, train_mape * 100))
-    log_string(log, 'val              %.2f\t\t%.2f\t\t%.2f%%' %
+    log_string(log, 'Validation       %.2f\t\t%.2f\t\t%.2f%%' %
                (val_mae, val_rmse, val_mape * 100))
-    log_string(log, 'test             %.2f\t\t%.2f\t\t%.2f%%' %
+    log_string(log, 'Test             %.2f\t\t%.2f\t\t%.2f%%' %
                (test_mae, test_rmse, test_mape * 100))
-    log_string(log, 'performance in each prediction step')
-    
+    log_string(log, 'Performance in each prediction step')
+
+    # Initialize lists to store metrics for each prediction step
     MAE, RMSE, MAPE = [], [], []
 
+    # Calculate metrics for each prediction step
     for step in range(args.num_pred):
         mae, rmse, mape = metric(testPred[:, step], testY[:, step])
         MAE.append(mae)
         RMSE.append(rmse)
         MAPE.append(mape)
-        log_string(log, 'step: %02d         %.2f\t\t%.2f\t\t%.2f%%' %
-               (step + 1, mae, rmse, mape * 100))
+        log_string(log, 'Step: %02d         %.2f\t\t%.2f\t\t%.2f%%' %
+                   (step + 1, mae, rmse, mape * 100))
 
     # Convert lists to tensors
     MAE_tensor = torch.tensor(MAE, device=device)
--- basic/model/train.py
+++ basic/model/train.py
@@ -6,52 +6,38 @@ from utils.utils_ import log_string
 from model.model_ import *
 from utils.utils_ import load_data
 
-def get_free_cuda_device():
+def get_free_cuda_device(args):
     # Check if GPU is available
     if torch.cuda.is_available():
-        device_count = torch.cuda.device_count()
-        if device_count > 0:
-            # Iterate over available devices to find the one with the most free memory
-            free_memory = [torch.cuda.memory_reserved(i) - torch.cuda.memory_allocated(i) for i in range(device_count)]
-            device_index = free_memory.index(max(free_memory))
-            device = torch.device(f'cuda:{device_index}')
-            return device
-        else:
-            return torch.device('cpu')  # No CUDA devices available, use CPU
+        # Set device to the specified CUDA device
+        return torch.device('cuda', args.cuda_device)
     else:
-        return torch.device('cpu')  # CUDA not available, use CPU
+        # CUDA not available, use CPU
+        return torch.device('cpu')
+
 
 def train(model, args, log, loss_criterion, optimizer, scheduler):
-
-     # Get the free CUDA device
-    device = get_free_cuda_device()
-    if device.type == 'cuda':
-        log_string(log, f'Using CUDA device: {torch.cuda.get_device_name(device)}')
-    else:
-        log_string(log, 'No CUDA devices available, using CPU for computation')
+    # Get the free CUDA device
+    device = get_free_cuda_device(args)
 
     # Move model to GPU if available
     model.to(device)
-    #print("Model moved to GPU")  # Print statement
-    #log_string(log, "Model moved to GPU")  # Logging statement
+    # Move loss criterion to GPU if available
     loss_criterion.to(device)
 
+    # Load data
     (trainX, trainTE, trainY, valX, valTE, valY, testX, testTE,
      testY, SE, mean, std) = load_data(args)
-    # load data
-    log_string(log, 'data loaded!...')
-
+    log_string(log, 'Data loaded!...')
 
     num_train, _, num_vertex = trainX.shape
-    log_string(log, '**** training model ****')
-     
+    log_string(log, '**** Training model ****')
+
     num_val = valX.shape[0]
-    
+
     train_num_batch = math.ceil(num_train / args.batch_size)
     val_num_batch = math.ceil(num_val / args.batch_size)
 
-
-    
     wait = 0
     val_loss_min = float('inf')
     best_model_wts = None
@@ -59,6 +45,7 @@ def train(model, args, log, loss_criterion, optimizer, scheduler):
     val_total_loss = []
 
     if torch.cuda.is_available():
+        # Move data to GPU
         trainX = trainX.to(device)
         trainTE = trainTE.to(device)
         trainY = trainY.to(device)
@@ -68,67 +55,56 @@ def train(model, args, log, loss_criterion, optimizer, scheduler):
         testX = testX.to(device)
         testTE = testTE.to(device)
         testY = testY.to(device)
-        SE= SE.to(device)
+        SE = SE.to(device)
         mean = mean.to(device)
         std = std.to(device)
-        log_string(log, "Data moved to GPU")  # Logging statement
+        log_string(log, "Data moved to GPU")
 
-    # Train & validation
+    # Training & Validation
     for epoch in range(args.max_epoch):
         if wait >= args.patience:
-            log_string(log, f'early stop at epoch: {epoch:04d}')
+            log_string(log, f'Early stop at epoch: {epoch:04d}')
             break
-        # shuffle
-        permutation = torch.randperm(num_train)
 
-         
+        # Shuffle training data
+        permutation = torch.randperm(num_train)
         trainX = trainX[permutation]
-        
         trainTE = trainTE[permutation]
         trainY = trainY[permutation]
-        # train
+
+        # Train
         start_train = time.time()
         model.train()
-        
         train_loss = 0
         for batch_idx in range(train_num_batch):
             start_idx = batch_idx * args.batch_size
-            
             end_idx = min(num_train, (batch_idx + 1) * args.batch_size)
             X = trainX[start_idx: end_idx]
             TE = trainTE[start_idx: end_idx]
-            
             label = trainY[start_idx: end_idx]
-            
             optimizer.zero_grad()
-
-
             X = X.to(device)
             TE = TE.to(device)
-
-            
             pred = model(X, TE)
-            print ("checkpoint")
-            
             pred = pred * std + mean
             loss_batch = loss_criterion(pred, label)
             train_loss += float(loss_batch) * (end_idx - start_idx)
             loss_batch.backward()
             optimizer.step()
-            
-            
 
             if torch.cuda.is_available():
-                print ("checkpoint - inside")
                 torch.cuda.empty_cache()
+
             if (batch_idx+1) % 5 == 0:
                 print(f'Training batch: {batch_idx+1} in epoch:{epoch}, training batch loss:{loss_batch:.4f}')
+
             del X, TE, label, pred, loss_batch
+
         train_loss /= num_train
         train_total_loss.append(train_loss)
         end_train = time.time()
 
-        # val loss
+        # Validation loss
         start_val = time.time()
         val_loss = 0
         model.eval()
@@ -144,40 +120,28 @@ def train(model, args, log, loss_criterion, optimizer, scheduler):
                 loss_batch = loss_criterion(pred, label)
                 val_loss += loss_batch * (end_idx - start_idx)
                 del X, TE, label, pred, loss_batch
+
         val_loss /= num_val
         val_total_loss.append(val_loss)
         end_val = time.time()
-        log_string(
-            log,
-            '%s | epoch: %04d/%d, training time: %.1fs, inference time: %.1fs' %
-            (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), epoch + 1,
-             args.max_epoch, end_train - start_train, end_val - start_val))
-        log_string(
-            log, f'train loss: {train_loss:.4f}, val_loss: {val_loss:.4f}')
+        log_string(log, '%s | epoch: %04d/%d, training time: %.1fs, inference time: %.1fs' %
+                   (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), epoch + 1,
+                    args.max_epoch, end_train - start_train, end_val - start_val))
+        log_string(log, f'train loss: {train_loss:.4f}, val_loss: {val_loss:.4f}')
+
         if val_loss <= val_loss_min:
-            log_string(log,f'val loss decrease from {val_loss_min:.4f} to {val_loss:.4f}, saving model to {args.model_file}')
+            log_string(log, f'val loss decrease from {val_loss_min:.4f} to {val_loss:.4f}, saving model to {args.model_file}')
             wait = 0
             val_loss_min = val_loss
             best_model_wts = model.state_dict()
-            torch.save(best_model_wts, 'best_model_weights.pt')
+            torch.save(best_model_wts, './basic/data/best_model_weights.pt')
         else:
             wait += 1
+
         scheduler.step()
-	
-	
+
     model.load_state_dict(best_model_wts)
     torch.save(model, args.model_file)
-        
-    log_string(log, f'Training and validation are completed, and model has been stored as {args.model_file}')	
+
+    log_string(log, f'Training and validation are completed, and model has been stored as {args.model_file}')
     return train_total_loss, val_total_loss
-        
-	
-	## using torchscript
-
-    #model_scripted = torch.jit.script(model)  # Convert the trained model to a scripted module
-    #model_scripted.save(args.model_file)
-   
-	
-
-
-    
